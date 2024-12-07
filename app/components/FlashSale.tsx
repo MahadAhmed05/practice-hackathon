01c5e36d273c10fca21c9fbb282863716916cc31
MahadAhmed05 <ahmedmahad531@gmail.com>
2024-12-07 22:08:12 +0500

    replaced ' with empty space

--- app/components/FlashSale.tsx
+++ app/components/FlashSale.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import Card1 from "../images/Frame 611.png"
+import Card1 from "../images/Frame 611.png";
 function FlashSale() {
   return (
     <div className="w-[1308px] h-[493px] mx-auto p-4">
@@ -7,7 +7,7 @@ function FlashSale() {
       <div className="flex justify-between items-center mb-6">
         {/* Left Section: "Today's" and Countdown */}
         <div className="ml-10">
-          <h2 className="text-2xl font-bold text-red-600 mb-4">Today's</h2>
+          <h2 className="text-2xl font-bold text-red-600 mb-4">Todays</h2>
           <div className="flex gap-4 text-center">
             {/* Countdown Timer */}
             <div>
@@ -44,7 +44,9 @@ function FlashSale() {
       <div className="grid grid-cols-5 gap-4">
         {/* Product Card */}
         <div className="bg-gray-100 p-4 rounded-md shadow">
-          <div className="w-full h-36 bg-gray-300 mb-4"><img className="w-full h-full" src={Card1.src} alt="" /></div>
+          <div className="w-full h-36 bg-gray-300 mb-4">
+            <img className="w-full h-full" src={Card1.src} alt="" />
+          </div>
           <p className="text-sm font-bold">HAVIT HV-G92 Gamepad</p>
           <p className="text-red-600">
             $120 <span className="text-gray-500 line-through">$160</span>
